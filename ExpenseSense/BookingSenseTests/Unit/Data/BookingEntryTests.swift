166c507f71cafe919efdc64fd04564f0fbcc0470
Kenny S. <chillSeCdW@users.noreply.github.com>
2024-06-04 23:42:17 +0200

    fix unit tests

--- ExpenseSense/BookingSenseTests/Unit/Data/BookingEntryTests.swift
+++ ExpenseSense/BookingSenseTests/Unit/Data/BookingEntryTests.swift
@@ -19,7 +19,13 @@ final class BookingEntryTests: XCTestCase {
   }
 
   func testExpenseEntryCreation() throws {
-    let entry = BookingEntry(name: "someString", amount: Decimal(), amountPrefix: .minus, interval: .annually)
+    let entry = BookingEntry(
+      name: "someString",
+      tags: ["default"],
+      amount: Decimal(),
+      amountPrefix: .minus,
+      interval: .annually
+    )
 
     XCTAssertNotNil(entry.id)
     XCTAssertEqual(entry.name, "someString")
@@ -38,6 +44,7 @@ final class BookingEntryTests: XCTestCase {
     let objectString = """
     {
       "name" : "someString",
+      "tags" : ["default"],
       "amount" : 0,
       "id" : "CDE289A7-6293-4792-A57B-A4950D8E1C11",
       "amountPrefix" : {
@@ -54,6 +61,7 @@ final class BookingEntryTests: XCTestCase {
 
     XCTAssertEqual(entry.id, "CDE289A7-6293-4792-A57B-A4950D8E1C11")
     XCTAssertEqual(entry.name, "someString")
+    XCTAssertEqual(entry.tags[0], "default")
     XCTAssertEqual(entry.amount, 0)
     XCTAssertEqual(entry.amountPrefix, .minus)
     XCTAssertEqual(entry.interval, "jährlich")
@@ -62,7 +70,13 @@ final class BookingEntryTests: XCTestCase {
   func testExpenseEntryEncode() throws {
     let jsonEncoder = JSONEncoder()
     jsonEncoder.outputFormatting = .prettyPrinted
-    let entry = BookingEntry(name: "someString", amount: Decimal(), amountPrefix: .minus, interval: .annually)
+    let entry = BookingEntry(
+      name: "someString",
+      tags: ["default"],
+      amount: Decimal(),
+      amountPrefix: .minus,
+      interval: .annually
+    )
     let data = try jsonEncoder.encode(entry)
     print(String(data: data, encoding: .utf8)!)
 
@@ -71,13 +85,15 @@ final class BookingEntryTests: XCTestCase {
 
   func testTotalExpenseEntries() {
     let entry = BookingEntry(name: "someEntry",
+                             tags: ["default"],
                              amount: Decimal(500),
                              amountPrefix: AmountPrefix.minus,
                              interval: Interval.annually)
     let entry2 = BookingEntry(name: "someEntry",
-                             amount: Decimal(200),
-                             amountPrefix: AmountPrefix.minus,
-                             interval: Interval.annually)
+                              tags: ["default"],
+                              amount: Decimal(200),
+                              amountPrefix: AmountPrefix.minus,
+                              interval: Interval.annually)
     context.insert(entry)
     context.insert(entry2)
 
