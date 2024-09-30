d70e24a6454b4fa4de319d59af93b635bf480531
Kenny S. <chillSeCdW@users.noreply.github.com>
2024-09-30 16:39:43 +0200

    added more settings options, added Donations

--- BookingSense/Views/Settings/TipJarView.swift
+++ BookingSense/Views/Settings/TipJarView.swift
@@ -2,31 +2,50 @@
 // Using Swift 5.0
 
 import SwiftUI
+import StoreKit
+import OSLog
+
+private let logger = Logger(subsystem: "BookingSense", category: "TipJarView")
 
 struct TipJarView: View {
+    @Environment(\.colorScheme) var colorScheme
+
     var body: some View {
-      List {
-        Button {
-
-        } label: {
-          Text("🙂 Great tip")
+      StoreView(ids: ["com.chill.BookingSense.tip01",
+                      "com.chill.BookingSense.tip02",
+                      "com.chill.BookingSense.tip03",
+                      "com.chill.BookingSense.tip04"])
+        .productViewStyle(.compact)
+        .storeButton(.hidden, for: .cancellation)
+        .onInAppPurchaseStart { purchase in
+          print("In-App Purchase Started: \(purchase)")
         }
-        Button {
-
-        } label: {
-          Text("🤗 Amazing tip")
+        .onInAppPurchaseCompletion { _, result in
+          if case .success(.success(let verificationResult)) = result {
+            switch verificationResult {
+            case .verified(let transaction):
+                logger.debug("""
+                Transaction ID \(transaction.id) for \(transaction.productID) is verified
+                """)
+                await transaction.finish()
+                ThankYouPopUp(colorScheme: colorScheme)
+                  .showAndStack()
+                  .dismissAfter(2)
+                return
+            case .unverified(let transaction, let error):
+                logger.error("""
+                Transaction ID \(transaction.id) for \(transaction.productID) is unverified: \(error)
+                """)
+                return
+            }
+          }
+          if case .failure(let err) = result {
+            logger.error("""
+            Transaction failed with: \(err)
+            """)
+            return
+          }
         }
-        Button {
-
-        } label: {
-          Text("🤯 Extraordinary tip")
-        }
-        Button {
-
-        } label: {
-          Text("🫨 Ludicrous tip")
-        }
-      }.navigationTitle("Tips")
     }
 }
 
---
+++ BookingSense/Views/Toast/ThankYouPopUp.swift
@@ -0,0 +1,44 @@
+// Created for BookingSense on 30.09.24 by kenny
+// Using Swift 6.0
+
+import SwiftUI
+import MijickPopupView
+
+struct ThankYouPopUp: TopPopup {
+  let colorScheme: ColorScheme
+
+  func configurePopup(popup: TopPopupConfig) -> TopPopupConfig {
+    let scene = (UIApplication.shared.connectedScenes.first as? UIWindowScene)
+    let safeAreaPaddingTop = scene?.windows.first?.safeAreaInsets.top
+
+    return popup
+      .backgroundColour(Constants.getBackground(colorScheme))
+      .topPadding(safeAreaPaddingTop ?? 55)
+      .horizontalPadding(16)
+  }
+
+  func createContent() -> some View {
+    VStack(spacing: 12) {
+      HStack(spacing: 12) {
+        Image(systemName: "info.circle")
+          .foregroundColor(Color.blue)
+        VStack(alignment: .leading) {
+          Text(String(localized: "Wuhoo!"))
+            .fontWeight(.semibold)
+          Text(String(localized: "Thank you for your tip!"))
+        }.fixedSize(horizontal: false, vertical: true)
+      }
+      Button(action: dismiss) {
+        Text("Dismiss")
+      }
+    }
+    .padding(.top, 20)
+    .padding(.bottom, 16)
+    .padding(.leading, 20)
+    .padding(.trailing, 32)
+  }
+}
+
+#Preview {
+  ThankYouPopUp(colorScheme: ColorScheme.light)
+}
