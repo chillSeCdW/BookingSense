e2590d17d892bb00bdf0582921c71ca55fd1544b
Kenny S. <chillSeCdW@users.noreply.github.com>
2025-03-18 11:29:02 +0100

    refactor of files

---
+++ BookingSense/Views/Statistics/Timeline/TimelineStatsView.swift
@@ -0,0 +1,13 @@
+// Created for BookingSense on 18.03.25 by kenny
+// Using Swift 6.0
+
+import Foundation
+import SwiftUI
+import SwiftData
+import BookingSenseData
+
+struct TimelineStatsView: View {
+  var body: some View {
+    Text("Timeline View Content")
+  }
+}
